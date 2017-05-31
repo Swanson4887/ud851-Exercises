85e3ed25a2a5d54afddbd8f464fb37d9e42b13e4
Swanson4887 <mswentze@stevens.edu>
2017-05-31 15:00:15 -0400

    completed exercise 5-1

--- Lesson05a-Android-Lifecycle/T05a.01-Exercise-LogLifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.01-Exercise-LogLifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -47,20 +47,17 @@ public class MainActivity extends AppCompatActivity {
 
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
-        // TODO (1) Use logAndAppend within onCreate
-    }
+        private static final String TAG = MainActivity.class.getSimpleName();
 
-    // TODO (2) Override onStart, call super.onStart, and call logAndAppend with ON_START
-
-    // TODO (3) Override onResume, call super.onResume, and call logAndAppend with ON_RESUME
-
-    // TODO (4) Override onPause, call super.onPause, and call logAndAppend with ON_PAUSE
-
-    // TODO (5) Override onStop, call super.onStop, and call logAndAppend with ON_STOP
-
-    // TODO (6) Override onRestart, call super.onRestart, and call logAndAppend with ON_RESTART
-
-    // TODO (7) Override onDestroy, call super.onDestroy, and call logAndAppend with ON_DESTROY
+    /* Constant values for the names of each respective lifecycle callback */
+        private static final String ON_CREATE = "onCreate";
+        private static final String ON_START = "onStart";
+        private static final String ON_RESUME = "onResume";
+        private static final String ON_PAUSE = "onPause";
+        private static final String ON_STOP = "onStop";
+        private static final String ON_RESTART = "onRestart";
+        private static final String ON_DESTROY = "onDestroy";
+        private static final String ON_SAVE_INSTANCE_STATE = "onSaveInstanceState";
 
     /**
      * Logs to the console and appends the lifecycle method name to the TextView so that you can
